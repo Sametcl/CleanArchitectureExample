4c27bd4046da6723894fe1f4e78e8f939e9e26a1
Sametcl <samet2016.2022@gmail.com>
2025-10-06 17:19:13 +0300

    presentation katmaninda ki controlleri program.cs e tanittik ve persistance katmaninda dbcontext olusturduk . domain katmaninda entity ve baseEntity olusturduk.

--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -1,15 +1,22 @@
+using CleanArchitecture.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+string connectionString = builder.Configuration.GetConnectionString("SqlServer");   
+builder.Services.AddDbContext<AppDbContext>(options =>{
+    options.UseSqlServer(connectionString);
+});
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers()
+    //Presentation katmaninda controller yazmak icin bu sekilde program.cs'e tanitiyoruz.
+    .AddApplicationPart(typeof(CleanArchitecture.Presentation.AssemblyReference).Assembly);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
