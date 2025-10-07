a1369d1f5e48216a877364ba2bcaec1d410d0c5e
Sametcl <samet2016.2022@gmail.com>
2025-10-07 19:52:37 +0300

    cqrs yapisi kuruldu , mediatr ile handler command ve response yapisi yazildi . ekleme service ksiimlari yazildi.

---
+++ CleanArchitecture.Persistance/Services/CarService.cs
@@ -0,0 +1,29 @@
+﻿using CleanArchitecture.Application.Features.CarFeatures.Commands.CreateCar;
+using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Persistance.Context;
+
+namespace CleanArchitecture.Persistance.Services
+{
+    public sealed class CarService : ICarService
+    {
+        private readonly AppDbContext context;
+
+        public CarService(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task CreateAsync(CreateCarCommand request, CancellationToken cancellationToken)
+        {
+            Car car = new ()
+            {
+               Name = request.Name,
+               Model = request.Model,
+               EnginePower =request.EnginePower,
+            };
+            await context.Set<Car>().AddAsync(car, cancellationToken);   
+            await context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -1,16 +1,32 @@
+using CleanArchitecture.Application.Services;
 using CleanArchitecture.Persistance.Context;
+using CleanArchitecture.Persistance.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-string connectionString = builder.Configuration.GetConnectionString("SqlServer");   
-builder.Services.AddDbContext<AppDbContext>(options =>{
+//scope islemleri 
+builder.Services.AddScoped<ICarService, CarService>();
+
+
+
+//connection islemleri 
+string connectionString = builder.Configuration.GetConnectionString("SqlServer");
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
     options.UseSqlServer(connectionString);
 });
 
 builder.Services.AddControllers()
     //Presentation katmaninda controller yazmak icin bu sekilde program.cs'e tanitiyoruz.
     .AddApplicationPart(typeof(CleanArchitecture.Presentation.AssemblyReference).Assembly);
+
+//Cqrs pattern kullanmak icin MediatR kutuphanesini ekliyoruz ve tum handler'lari tarayip kayit ediyoruz. 
+builder.Services.AddMediatR(cfr =>
+{
+    cfr.RegisterServicesFromAssembly(typeof(CleanArchitecture.Application.AssemblyReference).Assembly);
+});
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
