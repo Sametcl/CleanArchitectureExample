fb616a0d0529b9e630696e14a5e0e1a56f08f82e
Sametcl <samet2016.2022@gmail.com>
2025-10-08 21:35:10 +0300

    car controllerda create car endpointi yazildi

---
+++ CleanArchitecture.Presentation/Abstractions/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Presentation.Abstractions
+{
+    [ApiController] 
+    [Route("api/[controller]/")]
+    public abstract class ApiController : ControllerBase
+    {
+        public readonly IMediator _mediator;
+
+        public ApiController(IMediator _mediator)
+        {
+            this._mediator = _mediator;
+        }
+    }
+}
