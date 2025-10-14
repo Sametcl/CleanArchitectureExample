49df964562537f2b07cce649375a4cee93336c2b
Sametcl <samet2016.2022@gmail.com>
2025-10-14 14:24:26 +0300

    create token metotu yazdildi . token icinde hangi verilerin tutulacagi eklendi .

---
+++ CleanArchitecture.Infrastructure/Authentication/JwtProvider.cs
@@ -0,0 +1,43 @@
+﻿using CleanArchitecture.Application.Abstractions;
+using CleanArchitecture.Domain.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CleanArchitecture.Infrastructure.Authentication
+{
+    public sealed class JwtProvider : IJwtProvider
+    {
+        private readonly JwtOptions jwtOptions;
+
+        public JwtProvider(IOptions<JwtOptions> jwtOptions)
+        {
+            this.jwtOptions = jwtOptions.Value;
+        }
+
+        public string CreateToken(User user)
+        {
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.Email , user.Email),
+                new Claim(JwtRegisteredClaimNames.Name , user.UserName),
+                new Claim("UserName" , (user.FirstName + user.LastName)),
+            };
+            JwtSecurityToken jwtSecurityToken = new(
+                issuer: jwtOptions.Issuer,
+                audience: jwtOptions.Audience,
+                claims: claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(jwtOptions.SecretKey)),
+                    SecurityAlgorithms.HmacSha256));
+
+            string token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+            return token;
+        }
+    }
+}
